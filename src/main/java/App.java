ef1d00daeb415515d70555143813911f517aa8d0
Nevsski <amosovivan02@gmail.com>
2024-11-26 14:50:06 +0300

    modified task

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,21 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        bean3.setMessage("cat1");
+        System.out.println(bean2.getMessage());
+
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+        bean4.setMessage("cat2");
+        System.out.println(bean2.getMessage());
+        boolean test = bean == bean2;
+        boolean test2 = bean3 == bean4;
+        System.out.println(test);
+        System.out.println(test2);
     }
 }
